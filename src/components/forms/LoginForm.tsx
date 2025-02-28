aabf65dca6e72918cf4106c8d3f2c00e171e7b35
Anghelo Alva <ance.anghelo@gmail.com>
2025-02-28 20:55:04 -0300

    Update loading spinner styles in LoginForm and RegisterForm components

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -112,7 +112,7 @@ export default function LoginForm(): React.ReactElement {
 						<div role="status" className="flex items-center justify-center">
 							<svg
 								aria-hidden="true"
-								className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+								className="fill-primary h-8 w-8 animate-spin text-gray-200 dark:text-gray-600"
 								viewBox="0 0 100 101"
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
