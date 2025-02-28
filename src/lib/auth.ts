0ac43b0da23b834e6f0b0e1f4157db62b998ae06
Anghelo Alva <ance.anghelo@gmail.com>
2025-02-28 18:08:29 -0300

    Implement authentication  with better-auth and database setup with Prisma

---
+++ src/lib/auth.ts
@@ -0,0 +1,39 @@
+import { nextCookies } from "better-auth/next-js"
+import { admin } from "better-auth/plugins"
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import prisma from "./prisma"
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+		// schema: {
+		// 	user,
+		// 	account,
+		// 	session,
+		// 	verification,
+		// },
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60,
+		},
+	},
+	user: {
+		additionalFields: {
+			rut: {
+				type: "string",
+				required: true,
+				unique: true,
+			},
+		},
+	},
+	plugins: [nextCookies(), admin()],
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
+})
+
+export type Session = typeof auth.$Infer.Session
