f4177aa545450a7fc7cef904fa6ffb3f980419a4
Anghelo Alva <ance.anghelo@gmail.com>
2025-04-09 17:43:38 -0400

    Simplify Prisma client initialization and remove auto-disconnect logic

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,37 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 
-const prismaClientSingleton = () => {
-	const client = new PrismaClient({
-		datasources: {
-			db: {
-				url: process.env.DATABASE_URL,
-			},
-		},
-		log: ["error", "warn"],
-	}).$extends({
-		name: "connection-manager",
-		client: {
-			afterRequest: async () => {
-				if (process.env.NODE_ENV === "production") {
-					// Desconectar después de cada operación en producción
-					setTimeout(() => {
-						client.$disconnect()
-					}, 200)
-				}
-			},
-		},
-	})
+const prisma = new PrismaClient()
 
-	return client
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
