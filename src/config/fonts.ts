cacd0afca920d0fa957541e52183f69fce30d740
Anghelo Alva <ance.anghelo@gmail.com>
2025-08-12 00:03:18 -0400

    Delete unused fonts

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,19 +1,4 @@
-import { Geist, Geist_Mono, Space_Grotesk, Manrope } from "next/font/google"
-
-export const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-})
-
-export const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-})
-
-export const spaceGrotesk = Space_Grotesk({
-	variable: "--font-space-grotesk",
-	subsets: ["latin"],
-})
+import { Manrope } from "next/font/google"
 
 export const generalFont = Manrope({
 	variable: "--font-general",
